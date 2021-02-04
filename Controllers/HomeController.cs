1223d6c4e5dd1bebd34336c083c81ec5e4d0cc9e
Adriana Cottle <adriana.cottle@gmail.com>
2021-02-03 17:23:13 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult movieApp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult movieApp(AppResponse formResponse)
+        {
+            tempStorage.AddApplication(formResponse);
+            return View("confirmation", formResponse);
+        }
+
+        public IActionResult podcasts()
+        {
+            return View();
+        }
+
+        public IActionResult moviesList()
+        {
+            return View(tempStorage.Applications);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/tempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace movies.Models
+{
+    public static class tempStorage
+    {
+        private static List<AppResponse> applications = new List<AppResponse>();
+
+        public static IEnumerable<AppResponse> Applications => applications;
+
+        public static void AddApplication(AppResponse application)
+        {
+            applications.Add(application);
+        }
+    }
+}
